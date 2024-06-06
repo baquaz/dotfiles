ef9689d3877db1fc7d161dfb7f24deac0bbfd261
pb <9070569+baquaz@users.noreply.github.com>
2024-06-06 20:45:05 +0200

    add nvim configurations

---
+++ .config/nvim/lua/baquaz/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+
+    "nvim-telescope/telescope.nvim", -- optional
+  },
+  config = true,
+
+  config = function()
+    require("neogit").setup {
+      -- configure
+    }
+  end,
+}
